4d904b8d437e42d0d7b16bef0aeb23aba86ff6e3
Patricia C <patricia@patriciaco.com>
2020-01-27 06:18:56 +0000

    Done.

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,9 +1,18 @@
+#def plus_two(num)
+	 #num + 2
+	 #num
+	 #binding.pry
+	 #return num + 2
+#end
+
 def plus_two(num)
-	 num + 2
-	 num
-	 binding.pry
-	 return num + 2
+ num + 2
+ num
+ return num + 2
 end
+
+
+
 
 
 
